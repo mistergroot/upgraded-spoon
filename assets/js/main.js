94419a7962d02a6ef02bf8af6049c07ae4e80fb0
Nick Locatelli <nl532@cornell.edu>
2025-09-16 21:12:16 -0400

    updating popups

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,7 +266,7 @@ $(document).ready(function () {
         "pic01.jpg": `<h3 style="text-align: center;">A Survey of Climate in 
 		Adirondack Lake Ecosystems (SCALE)</h3>
 		<p style="text-align: justify;">At Cornell, I am using 
-		environmental DNA (eDNA) to understand 
+		environmental DNA (eDNA) metabarcoding to understand 
 		how aquatic communities in the Adirondack Park are changing in 
 		response to anthropogenic stressors. As part of this effort, 
 		I am also working to expand the genetic resources available 
@@ -276,10 +276,55 @@ $(document).ready(function () {
 		brook trout, as well as exploring the feasibility of assessing 
 		genetic health of threatened species such as round whitefish 
 		using nuclear eDNA.</p>`,
-        "pic02.jpg": `<h3 style="text-align: center;">Population Genomics of Critically Endangered Caribbean Corals</h3>
-		<p style="text-align: justify;">Details about project 2...</p>`,
-        "pic03.jpg": `<h3 style="text-align: center;">Unintentional Artificial Selection in Sex-based Coral Restoration</h3>
-		<p style="text-align: justify;">Details about project 3...</p>`,
+        "pic02.jpg": `<h3 style="text-align: center;">Genome Evolution of 
+		Critically Endangered Caribbean Corals</h3>
+		<p style="text-align: justify;">During my PhD, my research focused on 
+		exploring the genome evolution of Caribbean corals to 
+		better understand how the size, structure, and content of their genomes 
+		has led to the diversity of corals we observe today. 
+		For one study, I was part of a collaborative effort that constructed 
+		the first chromosome-scale genome assemblies and genetic maps for 
+		two critically endangered Atlantic corals, 
+		<em>Acropora palmata</em> and <em>A. cervicornis</em>. 
+		By comparing their genomes, 
+		we showed that these species maintain a high degree of macrosynteny 
+		(their genomes are largely the same in structure), despite millions 
+		of years of divergence, and that both have high genome-wide 
+		recombination rates, meaning that they both have a strong ability to 
+		shuffle their genes during sexual reproduction. In another project, 
+		I generated the first high-quality genomes for three additional 
+		Caribbean reef-builders — <em>Colpophyllia natans</em>, 
+		<em>Dendrogyra cylindrus</em>, and <em>Siderastrea siderea</em>. 
+		This work revealed surprising 
+		patterns of gene duplications and expansions, especially in 
+		<em>S. siderea</em>, which has one of the largest described coral genomes. 
+		Together, these studies provide foundational resources for 
+		conservation genetics and open the door to investigating how the unique 
+		evolutionary history of Caribbean corals may shape their ability to 
+		persist in rapidly changing oceans.</p>`,
+        "pic03.jpg": `<h3 style="text-align: center;">Genetic Bottlenecks 
+		in Coral Restoration</h3>
+		<p style="text-align: justify;">In coral restoration, the common 
+		assumption is that if you cross equal quantites of eggs and sperm of 
+		several parent colonies together, all parents will contribute 
+		equally to the offspring—a state called panmixia. In work with SECORE, 
+		we tested this assumption using four-parent crosses of 
+		<em>Acropora palmata</em> and <em>Colpophyllia natans</em>, 
+		raising larvae in both lab cultures and ocean enclosures and 
+		sequencing their DNA (Pool-Seq) to track which parents actually 
+		contributed over time. What we discovered is that this 
+		expectation of equal contributions does not hold: 
+		within just days, offspring in lab cultures became dominated 
+		by only a couple of parents, even though all were initially 
+		compatible. These early shifts mean that ex-situ breeding can 
+		create unintended genetic bottlenecks, reducing the 
+		diversity that restoration programs are trying to promote. 
+		We also found that certain genes were putatively responsible for 
+		differential survival in the lab and ocean enclosures. 
+		These results highlight that assisted breeding is not a 
+		neutral process, and that coral crosses need to be carefully designed 
+		and managed to preserve the adaptive potential of 
+		restored populations in changing oceans.</p>`,
         "pic04.jpg": `<h3 style="text-align: center;">Understanding the Globalization of the Aquaculture Using Metabarcoding of Feeds</h3>
 		<p style="text-align: justify;">Details about project 4...</p>`
         // add more as needed
